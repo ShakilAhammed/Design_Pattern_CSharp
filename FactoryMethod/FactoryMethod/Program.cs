a7367417edc60b9e1c8236892fc6b85d8b917d8f
Shakil Ahammed <shakil@gifty.asia>
2024-06-02 02:06:47 +0600

    singleton and factory pattern added

---
+++ FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,56 @@
+﻿public interface ICar
+{
+    public void Drive();
+}
+
+public class BMW: ICar
+{
+
+    public void Drive()
+    {
+        Console.WriteLine("wow, my BMW is dancing");
+    }
+}
+
+public class Van : ICar
+{
+
+    public void Drive()
+    {
+        Console.WriteLine("wow, my van is running");
+    }
+}
+
+public abstract class CarBuilder
+{
+    public abstract ICar BuildCar();
+}
+
+public class BMWBuilder : CarBuilder
+{
+    public override ICar BuildCar()
+    {
+        return new BMW();
+    }
+}
+
+public class VanBuilder : CarBuilder
+{
+    public override ICar BuildCar()
+    {
+        return new Van();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        CarBuilder carBuilder;
+        //depending on demand i can call any type of car builder
+        carBuilder = new BMWBuilder();
+        ICar myBMW = carBuilder.BuildCar();
+        myBMW.Drive();
+    }
+}
+
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+public sealed class Singleton
+{
+    private static Singleton instance = new Singleton();
+	private Singleton()
+	{
+
+	}
+
+	public static Singleton Instance {
+		get
+		{
+			return instance;
+		}
+	}
+
+	//do something
+	public void DoSomething()
+	{
+		Console.WriteLine("Doing.........");
+	}
+}
+
+public sealed class ThreadSafetySingleton
+{
+	//Lazy has lock implementation inside while creating the object. so we can use it efficiently to make it thread safe
+	private static Lazy<ThreadSafetySingleton> threadSafeInstance = new Lazy<ThreadSafetySingleton>(new ThreadSafetySingleton());
+	private ThreadSafetySingleton() { }
+
+	public static ThreadSafetySingleton Instance { get { return threadSafeInstance.Value;} }
+
+	//do something
+	public void DoSomething()
+	{
+		Console.WriteLine("Doing..........");
+	}
+}
