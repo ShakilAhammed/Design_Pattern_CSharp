b7f0d6c36031798184dc9032d949e8f9e5d573f4
shakil <shakil@shakil.shakil>
2024-08-03 13:49:11 +0600

    added chain of responsiblity pattern and i loved it

---
+++ ChainOfResponsiblity/Program.cs
@@ -0,0 +1,31 @@
+﻿abstract class SupportHandler{
+    public SupportHandler? nextHandler = null;
+    public abstract void Handle();
+    public void Proceed(){
+        this.Handle();
+        if(this.nextHandler != null) this.nextHandler.Proceed();
+    }
+}
+
+class LevelOneSupport : SupportHandler{
+    public override void Handle()
+    {
+        Console.WriteLine("Level one support");
+    }
+}
+
+class LevelTwoSupport : SupportHandler{
+    public override void Handle()
+    {
+        Console.WriteLine("Level two support");
+    }
+}
+
+static class Program{
+    public static void Main(){
+        LevelOneSupport levelOneSupport = new LevelOneSupport();
+        LevelTwoSupport levelTwoSupport = new LevelTwoSupport();
+        levelOneSupport.nextHandler = levelTwoSupport;
+        levelOneSupport.Proceed();
+    }
+}
