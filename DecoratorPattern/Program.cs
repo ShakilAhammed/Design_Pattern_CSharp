f0299349ab92895416827533ea4a8278b94fa5fb
Shakil Ahammed <shakilinfo7@gmail.com>
2024-08-24 14:42:15 +0600

    added Decorator Pattern and i loved it

---
+++ DecoratorPattern/Program.cs
@@ -0,0 +1,42 @@
+﻿public interface INotifier{
+    void SendMessage(string message);
+}
+
+public class NotifierDecorator : INotifier{
+    public INotifier _notifier;
+
+    public NotifierDecorator(INotifier notifier){
+        _notifier = notifier;
+    }
+
+    public virtual void SendMessage(string message){
+        Console.WriteLine(message);
+    }
+}
+
+public class EmailNotifier : INotifier
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine($"[Email] {message}");
+    }
+}
+
+public class SMSNotifier: NotifierDecorator
+{
+    public SMSNotifier(INotifier notifier) : base(notifier)
+    {
+    }
+
+    public override void SendMessage(string message){
+        Console.WriteLine($"[SMS] {message}");
+        _notifier.SendMessage(message);
+    }
+}
+
+public static class Program{
+    public static void Main(string[] arg){
+        INotifier notifier = new SMSNotifier(new EmailNotifier());
+        notifier.SendMessage("Hello World");
+    }
+}
