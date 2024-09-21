a3e8fdf52d1cf76249fb33fdbf5bdc666833844f
Shakil Ahammed <shakilinfo7@gmail.com>
2024-09-21 16:52:55 +0600

    please read README.md, loved this builder patterh

---
+++ Builder/Program.cs
@@ -0,0 +1,116 @@
+﻿public class Car{
+    public Transmission Transmission{ get;set; }
+    public string Engine { get; set; }
+    public bool HasGPS { get; set; }
+    public override string ToString()
+    {
+        return $"Car Specifications:\nEngine: {Engine}\nTransmission: {Transmission}\nGPS Included: {HasGPS}";
+    }
+}
+
+public enum Transmission{
+    Auto,
+    Manual
+}
+
+public interface ICarBuilder{
+    ICarBuilder SetEngine(string engine);
+    ICarBuilder SetTransmission(Transmission transmission);
+    ICarBuilder SetGPS(bool hasGPS);
+    Car Build();
+}
+
+public class SportsCarBuilder: ICarBuilder{
+    private Car Car = new Car();
+    public ICarBuilder SetEngine(string engine)
+    {
+        Car.Engine = engine;
+        return this;
+    }
+    public ICarBuilder SetTransmission(Transmission transmission)
+    {
+        Car.Transmission = transmission;
+        return this;
+    }
+    public ICarBuilder SetGPS(bool hasGPS)
+    {
+        Car.HasGPS = hasGPS;
+        return this;
+    }
+    public Car Build()
+    {
+        return Car;
+    }
+}
+
+public class BMWCarBuilder : ICarBuilder
+{
+    private Car _car = new Car();
+
+    public ICarBuilder SetEngine(string engine)
+    {
+        _car.Engine = engine;
+        return this;
+    }
+
+    public ICarBuilder SetTransmission(Transmission transmission)
+    {
+        _car.Transmission = transmission;
+        return this;
+    }
+
+    public ICarBuilder SetGPS(bool hasGPS)
+    {
+        _car.HasGPS = hasGPS;
+        return this;
+    }
+
+    public Car Build()
+    {
+        return _car;
+    }
+}
+
+
+public class CarDirector{
+    private ICarBuilder _carBuilder;
+    public CarDirector(ICarBuilder carBuilder)
+    {
+        _carBuilder = carBuilder;
+    }
+    public Car ConstructSportsCar()
+    {
+        return _carBuilder
+            .SetEngine("V8")
+            .SetTransmission(Transmission.Auto)
+            .SetGPS(true)
+            .Build();
+    }
+
+    public Car ConstructBMWCar()
+    {
+        return _carBuilder
+            .SetEngine("V6")
+            .SetTransmission(Transmission.Manual)
+            .SetGPS(false)
+            .Build();
+    }
+}
+
+public static class Program{
+    public static void Main(string[] args)
+    {
+        ICarBuilder sportsCarBuilder = new SportsCarBuilder();
+        CarDirector carDirector = new CarDirector(sportsCarBuilder);
+        Car sportsCar = carDirector.ConstructSportsCar();
+        Console.WriteLine("Sports Car Built:");
+        Console.WriteLine(sportsCar.ToString());
+
+        ICarBuilder bmwCarBuilder = new BMWCarBuilder();
+        carDirector = new CarDirector(bmwCarBuilder);
+        Car bmwCar = carDirector.ConstructBMWCar();
+        Console.WriteLine("\nBMW Car Built:");
+        Console.WriteLine(bmwCar.ToString());
+
+    }
+}
---
+++ Builder/README.md
@@ -0,0 +1,5 @@
+i loved this pattern to explore
+
+so what i have understood, basically if you have complex class(product we can say) to build, then instead of taking all the things inside constructor, build it sequentially or make it decouple cause sometimes we may not need all the things. right? so this builder pattern is useful when you have so many thing to build within a class, i mean to say we will get the vibe of building(connecting the dots) or orchestrating some thing. its intersting and fun.
+
+Through out my implementation you will get factory pattern as well, so much fun in one implementation. Beautiful!!!!!!
